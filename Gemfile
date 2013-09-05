67defe7f3402638672c34026173ab001e7a1f44c
Glenn Goodrich <glenn.goodrich@gmail.com>
2013-09-05 13:12:11 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+ruby '1.9.3', engine: 'jruby', engine_version: '1.7.4'
+gem 'rails', '3.2.12'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'activerecord-jdbcpostgresql-adapter'
+
+gem 'jruby-openssl'
+
+gem 'torquebox'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArJdbcTest::Application.config.secret_token = 'f6fbbc33804577b64c95e3ef75c1c787f70f1f2502b65e34616e9747dab1bab25fdc6f90924e39b7ed7c9773693af477f5c58905ca2395490fc7f19068c966a2'
